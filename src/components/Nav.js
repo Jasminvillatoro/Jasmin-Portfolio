965ca0eb0142a943f7a8e5cd6280154b77be5505
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-02-11 21:46:01 -0500

    Nav logo

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import logo from './Images/Jascodes-removebg.png';
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <img src={logo} alt="" style={{ height: '200px' }} />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
