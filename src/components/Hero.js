1e3fb7ea096c84bf34a372492b1d902a0b6ba1de
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-02-25 19:08:48 -0500

    deleted image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,9 @@ import heroimg from './Images/hero.webp';
 const Hero = () => {
   return (
     <section className="hero--cnt">
-      <div className="hero--img-cnt">
-        <img src={heroimg} alt="hero " />
-      </div>
-      <div className="hero--text-cnt">
-        <h1 className="hero--text">
-          Jasmin <br /> Frontend Developer
-        </h1>
-      </div>
+      <h1 className="hero--text">
+        Jasmin <br /> Frontend Developer
+      </h1>
     </section>
   );
 };
