0cb413290fe8173dee390d7e5773bca164b4ef29
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-03-01 20:27:54 -0500

    new images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,11 @@ const Projects = () => {
     <section>
       <div className="projects--container">
         <div>div1</div>
+        <div>div2</div>
+        <div>div3</div>
+        <div>div4</div>
+        <div>div5</div>
+        <div>div6</div>
       </div>
     </section>
   );
