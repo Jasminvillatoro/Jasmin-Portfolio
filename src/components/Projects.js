979a13076bdeb801ab5145bce3dc651bd81ef299
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-03-02 10:22:10 -0500

    setup ideal for project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 const Projects = () => {
   return (
     <section>
+      <h1>Projects</h1>
+      <p>A selection of my projects</p>
       <div className="projects--container">
         <div>div1</div>
         <div>div2</div>
