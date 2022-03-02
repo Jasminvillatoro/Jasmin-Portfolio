51c040bf7f5caf1a9ed478033e0fd17971f751c2
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-03-01 20:25:32 -0500

    background and text style change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="hero--cnt">
       <h1 className="hero--text">
-        Jasmin <br /> Frontend Developer
+        <span className="name">Jasmin🌷✨</span> <br /> Frontend Developer
       </h1>
     </section>
   );
