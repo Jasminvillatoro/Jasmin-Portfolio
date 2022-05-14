b59f2b222abad941d6928884c2e5d3cf37fe865d
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-05-13 22:27:56 -0400

    jsx deletion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 import project1 from './Images/project1.png';
 import project2 from './Images/portfolio.png';
 import project3 from './Images/card.png';
-import project4 from './Images/weather.png';
+
 import project5 from './Images/plants.png';
-import project6 from './Images/personal.png';
+
 const Projects = () => {
   return (
     <section id="projects">
@@ -35,22 +35,11 @@ const Projects = () => {
             / GitHub
           </a>
         </div>
-        <div>
-          <img className="portfolio-img" src={project4} alt="project1" />
-          <a href="https://jasminvillatoro.github.io/Weather-App/">Live </a>
-          <a href="https://github.com/Jasminvillatoro/Weather-App"> / GitHub</a>
-        </div>
+
         <div>
           <img className="portfolio-img" src={project5} alt="project1" />
           <a href="https://jasminvillatoro.github.io/Plants-101/">Live </a>
           <a href="https://github.com/Jasminvillatoro/Plants-101"> / GitHub</a>
-        </div>
-        <div>
-          <img className="portfolio-img" src={project6} alt="project1" />
-          <a href="https://jasminvillatoro.github.io/Personal-Website/">Live</a>
-          <a href="https://github.com/Jasminvillatoro/Personal-Website">
-            / GitHub
-          </a>
         </div>
       </div>
     </section>
