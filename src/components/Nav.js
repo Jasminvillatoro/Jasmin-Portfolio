deeb5fe4c966cec395ff8488f74d7405d873cb48
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-05-19 14:32:23 -0400

    added className to fix issue with the other a tags

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,16 +23,24 @@ const Nav = () => {
         </div>
         <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
           <li>
-            <a href="#home">Home</a>
+            <a className="navbar-a" href="#home">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a className="navbar-a" href="#about">
+              About
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a className="navbar-a" href="#projects">
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a className="navbar-a" href="#contact">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
