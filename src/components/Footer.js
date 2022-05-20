75b6958333b229c4bb66b8b8c608b4e3f195c7d3
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-05-20 11:16:18 -0400

    footer jsx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,50 @@
 import '../styles.css';
+import { FaTwitterSquare } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { IconContext } from 'react-icons';
 
 const Footer = () => {
   return (
-    <footer id="contact">
-      <div className="contact-info">
-        <a
-          className="footer-text"
-          href="mailto:jasminvilla321@gmail.com? subject=message from portfolio">
-          <p className="footer-text">
-            Click to contact me <br /> through email
-          </p>
-        </a>
-        <p className="footer-text">(954)-347-4053</p>
-        <p className="footer-text">&#169;Jasmin</p>
+    <footer>
+      <div className="links-cnt">
+        <h2>Links</h2>
+        <ul className="footer-ul">
+          <li className="footer-li">
+            <a className="footer-a" href="#home">
+              Home
+            </a>
+          </li>
+          <li className="footer-li">
+            <a className="footer-a" href="#about">
+              About
+            </a>
+          </li>
+          <li className="footer-li">
+            <a className="footer-a" href="#projects">
+              Projects
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="contact-icons">
+        <h2>Get in touch</h2>
+        <IconContext.Provider value={{ className: 'media-icons  ' }}>
+          <a href="https://www.linkedin.com/in/jasmin-villatoro/">
+            <FaLinkedin />
+          </a>
+          <a href="https://twitter.com/Jas_codes">
+            <FaTwitterSquare />
+          </a>
+          <a href="https://github.com/Jasminvillatoro">
+            <FaGithub />
+          </a>
+          <a href="mailto:jasminvilla321@gmail.com? subject=message from portfolio">
+            <HiOutlineMail />
+          </a>
+          <p>&#169;2022 created by Jasmin Villatoro</p>
+        </IconContext.Provider>
       </div>
     </footer>
   );
