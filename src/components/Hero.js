ae790b08b9a080f4834fb4d73f95a90e30d6baea
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-05-20 21:11:30 -0400

    moved iconcontext provider wrapper

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,30 +9,32 @@ import { IconContext } from 'react-icons';
 
 const Hero = () => {
   return (
-    <section id="home" className="hero--cnt">
-      <div className="hero-text-cnt">
-        <h1 className="name">Hi, I'm Jasmin</h1>
-        <h1 className="job-title">Frontend Web Developer</h1>
-        <IconContext.Provider value={{ className: 'media-icons' }}>
-          <a href="https://www.linkedin.com/in/jasmin-villatoro/">
-            <FaLinkedin />
-          </a>
-          <a href="https://twitter.com/Jas_codes">
-            <FaTwitterSquare />
-          </a>
-          <a href="https://github.com/Jasminvillatoro">
-            <FaGithub />
-          </a>
-          <a href="mailto:jasminvilla321@gmail.com? subject=message from portfolio">
-            <HiOutlineMail />
-          </a>
-        </IconContext.Provider>
-        <p>I love to level up my skills!</p>
-      </div>
-      <div className="hero-img-cnt">
-        <img className="hero-img" src={coder} alt="programmer" />
-      </div>
-    </section>
+    <IconContext.Provider value={{ className: 'media-icons' }}>
+      <section id="home" className="hero--cnt">
+        <div className="hero-text-cnt">
+          <h1 className="name">Hi, I'm Jasmin</h1>
+          <h1 className="job-title">Frontend Web Developer</h1>
+          <div className="flex-icons">
+            <a href="https://www.linkedin.com/in/jasmin-villatoro/">
+              <FaLinkedin />
+            </a>
+            <a href="https://twitter.com/Jas_codes">
+              <FaTwitterSquare />
+            </a>
+            <a href="https://github.com/Jasminvillatoro">
+              <FaGithub />
+            </a>
+            <a href="mailto:jasminvilla321@gmail.com? subject=message from portfolio">
+              <HiOutlineMail />
+            </a>
+          </div>
+          <p>I love to level up my skills!</p>
+        </div>
+        <div className="hero-img-cnt">
+          <img className="hero-img" src={coder} alt="programmer" />
+        </div>
+      </section>
+    </IconContext.Provider>
   );
 };
 
