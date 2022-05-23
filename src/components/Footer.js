d2fd0aaf0ce9dc6ff1190c0ca115d6f2c144b072
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-05-23 11:46:38 -0400

    moved Iconcontext provider for easier use

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,46 +7,48 @@ import { IconContext } from 'react-icons';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="links-cnt">
-        <h2>Links</h2>
-        <ul className="footer-ul">
-          <li className="footer-li">
-            <a className="footer-a" href="#home">
-              Home
+    <IconContext.Provider value={{ className: 'media-icons  ' }}>
+      <footer>
+        <div className="links-cnt">
+          <h2>Links</h2>
+          <ul className="footer-ul">
+            <li className="footer-li">
+              <a className="footer-a" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="#about">
+                About
+              </a>
+            </li>
+            <li className="footer-li">
+              <a className="footer-a" href="#projects">
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="contact-icons">
+          <h2>Get in touch</h2>
+          <div>
+            <a href="https://www.linkedin.com/in/jasmin-villatoro/">
+              <FaLinkedin />
             </a>
-          </li>
-          <li className="footer-li">
-            <a className="footer-a" href="#about">
-              About
+            <a href="https://twitter.com/Jas_codes">
+              <FaTwitterSquare />
             </a>
-          </li>
-          <li className="footer-li">
-            <a className="footer-a" href="#projects">
-              Projects
+            <a href="https://github.com/Jasminvillatoro">
+              <FaGithub />
             </a>
-          </li>
-        </ul>
-      </div>
-      <div className="contact-icons">
-        <h2>Get in touch</h2>
-        <IconContext.Provider value={{ className: 'media-icons  ' }}>
-          <a href="https://www.linkedin.com/in/jasmin-villatoro/">
-            <FaLinkedin />
-          </a>
-          <a href="https://twitter.com/Jas_codes">
-            <FaTwitterSquare />
-          </a>
-          <a href="https://github.com/Jasminvillatoro">
-            <FaGithub />
-          </a>
-          <a href="mailto:jasminvilla321@gmail.com? subject=message from portfolio">
-            <HiOutlineMail />
-          </a>
+            <a href="mailto:jasminvilla321@gmail.com? subject=message from portfolio">
+              <HiOutlineMail />
+            </a>
+          </div>
           <p>&#169;2022 created by Jasmin Villatoro</p>
-        </IconContext.Provider>
-      </div>
-    </footer>
+        </div>
+      </footer>
+    </IconContext.Provider>
   );
 };
 
